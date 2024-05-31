f9e5e0be9ff99abdaebc3570b1c424607f3b8f39
Sav <dsouvik141@gmail.com>
2024-06-01 01:18:32 +0530

    INIT

--- src/components/static-co/Section.jsx
+++ src/components/static-co/Section.jsx
@@ -5,5 +5,5 @@ export const Section = ({ className, children }) => {
   const mdw = useMediaQuery((theme) => theme.breakpoints.down('md'));
   const spacing = mdw ? 2 : 4;
   const size = children.length || 1;
-  return <div className={cn(`grid w-full grid-cols-2 gap-4 p-8`, className)}>{children}</div>;
+  return <div className={cn(`grid w-full grid-cols-2 gap-4 p-0 md:p-8`, className)}>{children}</div>;
 };
--- src/screens/ResourcePreview.jsx
+++ src/screens/ResourcePreview.jsx
@@ -3,12 +3,12 @@ import { createElement } from 'react';
 
 export const ResourcePreview = () => {
   return (
-    <div className="w-full p-8 overflow-auto">
+    <div className="w-full p-0 md:p-8 overflow-auto">
       <div className="flex flex-col items-start space-y-2">
         <p>RESOURCES</p>
         <div className="w-full grid grid-cols-3 gap-4">
           {resources.map(({ title, subtitle, tags = [], component }) => (
-            <ResourceCard title={title} subtitle={subtitle} tags={tags}>
+            <ResourceCard title={title} subtitle={subtitle} tags={tags} className="col-span-3 md:col-span-1">
               {createElement(component)}
             </ResourceCard>
           ))}
